d29a4c5100228bf78d03116fc3a9a9946376233b
Yongkeun Yun <yongkeun.yun@sk.com>
2023-10-17 16:27:47 +0900

    update timecomplex

--- tests/test_f_timecomplex.py
+++ tests/test_f_timecomplex.py
@@ -12,7 +12,7 @@ class TestAllCombinations(unittest.TestCase):
         self.assertTrue(duration < 1)  # 1초 이내에 완료되어야 함
     
     def test_short_input(self):
-        large_list = list(range(5))  # 길이가 20인 리스트
+        large_list = list(range(5))  # 길이가 5인 리스트
         start_time = time.time()
         all_combinations(large_list)
         duration = time.time() - start_time
